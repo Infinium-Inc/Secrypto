e5448aff340633ed18409daffd52bcdf73dec9da
Aahan Salecha <salecha.aahan@gmail.com>
2025-01-22 21:43:49 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+long_description = """
+
+"""
+
+setup(
+    name='secrypto',
+    version='0.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Aahan Salecha',
+    author_email='salecha.aahan@gmail.com',
+    description='A powerful encryption and decryption library',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='CC0 1.0 Universal',
+    project_urls={
+        'Source Repository': 'https://github.com/aahan0511/Secrypto/'
+    }
+)
